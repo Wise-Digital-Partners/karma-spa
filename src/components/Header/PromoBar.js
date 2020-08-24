3caaddc62a8d0eb0712231ab809c53bf535e6487
hamletavetisian <hamlet@wisedigitalpartners.com>
2020-08-23 18:26:47 -0700

    Update mobile header styles

--- src/components/Header/PromoBar.js
+++ src/components/Header/PromoBar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const PromoBar = () => {
     return(
-        <div id="promo-bar" className="bg-secondary_400 py-2 text-center">
+        <div id="promo-bar" className="bg-secondary_400 py-2 md:py-3 text-center">
             <div className="container">
-                <span className="text-white">Massage Deal for First-Timers: $49 for 60 min. | $69 for 90 min. Call for details.</span>
+                <p className="text-white text-small md:text-base mb-0">Massage Deal for First-Timers: $49 for 60 min. | $69 for 90 min. Call for details.</p>
             </div>
         </div>
     );
