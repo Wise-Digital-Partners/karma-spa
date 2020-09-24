1b2a2777fe726628cfccae4cdfdf0475639278d4
hamletavetisian <hamlet@wisedigitalpartners.com>
2020-09-23 17:03:06 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,26 +134,26 @@ module.exports = {
         icon: `static/images/favicon.png`,
       },
     },   
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        // id: "GTM-NZ2VW3R",
+    // {
+    //   resolve: "gatsby-plugin-google-tagmanager",
+    //   options: {
+    //     id: "GTM-NZ2VW3R",
   
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+    //     // Include GTM in development.
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: false,
   
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        // Defaults to null
-        // defaultDataLayer: { platform: "gatsby" },
+    //     // datalayer to be set before GTM is loaded
+    //     // should be an object or a function that is executed in the browser
+    //     // Defaults to null
+    //     // defaultDataLayer: { platform: "gatsby" },
   
-        // Specify optional GTM environment details.
-        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        // dataLayerName: "YOUR_DATA_LAYER_NAME",
-      },
-    },    
+    //     // Specify optional GTM environment details.
+    //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+    //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+    //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
+    //   },
+    // },    
     `gatsby-plugin-offline`
   ]
 };
