992336df2e7bc0266aa0cf8ab5d28299c2e5d48a
hamletavetisian <hamlet@wisedigitalpartners.com>
2020-10-23 16:11:15 -0700

    Add Google verification

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ children, headerStyle, headerLinkColor ,headerHasBorder }) => 
   return (
     <div>
       <Helmet>
-
+        <meta name="google-site-verification" content="wzt0TStiRMy-L7CUp0Iz5NQsQ7FzxvA2oGqUpxYAgI4" />
       </Helmet>
       <Header headerStyle={headerStyle} headerLinkColor={headerLinkColor} headerHasBorder={headerHasBorder} />
       <main id="body-content">
