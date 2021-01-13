658ddc3279f186ebef629c1e6cc46d34aad14e9c
hamletavetisian <hamlet@wisedigitalpartners.com>
2021-01-13 12:24:55 -0800

    Update skin-care-services.js

--- src/pages/skin-care-services.js
+++ src/pages/skin-care-services.js
@@ -53,38 +53,76 @@ export const Page = ({data}) => {
 
       <ScrollWrapper>
 		<ScrollNavigation>
-			<li><AnchorLink to="/skin-care-services/#section-1" title="Karma Spa Signature Facial" stripHash /></li>
+			<li><AnchorLink to="/skin-care-services/#section-1" title="Signature Facial" stripHash /></li>
 			<li><AnchorLink to="/skin-care-services/#section-2" title="O2 Facial" stripHash /></li>
 			<li><AnchorLink to="/skin-care-services/#section-3" title="Oxygen Facial" stripHash /></li>
 			<li><AnchorLink to="/skin-care-services/#section-4" title="HydraFacial" stripHash /></li>
 			<li><AnchorLink to="/skin-care-services/#section-5" title="Back Facial" stripHash /></li>
-			<li><AnchorLink to="/skin-care-services/#section-6" title="Clarity & Led Therapy (Hillcrest only)" stripHash /></li>
-			<li><AnchorLink to="/skin-care-services/#section-7" title="Customized Facial (Hillcrest only)" stripHash /></li>
-			<li><AnchorLink to="/skin-care-services/#section-8" title="Gentlemen’s Express Facial" stripHash /></li>			
-			<li><AnchorLink to="/skin-care-services/#section-9" title="Waxing" stripHash /></li>
-			<li><AnchorLink to="/skin-care-services/#section-10" title="Full Spectrum Infrared Sauna" stripHash /></li>
-			<li><AnchorLink to="/skin-care-services/#section-11" title="Sauna & Steam Room (SD)" stripHash /></li>
+			<li><AnchorLink to="/skin-care-services/#section-6" title="Mini Facial" stripHash /></li>
+			<li><AnchorLink to="/skin-care-services/#section-7" title="Clarity & Led Therapy (Hillcrest only)" stripHash /></li>
+			<li><AnchorLink to="/skin-care-services/#section-8" title="Customized Facial (Hillcrest only)" stripHash /></li>
+			<li><AnchorLink to="/skin-care-services/#section-9" title="Gentleman’s Facial" stripHash /></li>			
+			<li><AnchorLink to="/skin-care-services/#section-10" title="Waxing" stripHash /></li>
+			<li><AnchorLink to="/skin-care-services/#section-11" title="Steam Room (Hillcrest Only)" stripHash /></li>
 			<li><AnchorLink to="/skin-care-services/#section-12" title="Facial Add-ons" stripHash /></li>
 		</ScrollNavigation>		  
 		<ScrollContent>
 			<div id="section-1" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">01</span>
-				<h3>Karma Spa Signature Facial</h3>
-				<p>Our Signature Facial is a relaxing deep cleansing facial. Cleanses, exfoliates, and nourishes the skin, promoting a clear, well-hydrated complexion.</p>
+				<h3>Signature Facial</h3>
+				<p>Our Signature Facial is customizable to all skin types. It promotes a clear, well-hydrated complexion utilizing all-natural Dr. Schwab products. </p>
+				<ul className="styled-list mb-8">
+					<li>Skin analysis</li>
+					<li>Cleanse</li>
+					<li>Exfoliation</li>
+					<li>Extractions </li>
+					<li>Facial Massage</li>
+					<li>Mask Treatment</li>
+					<li>Arm & Hand Massage</li>
+					<li>Sunscreen</li>
+					<li>Toner & Moisturizer</li>
+					<li>Lip & Eye Treatment</li>
+				</ul>
+				<p>*Steam & hot towels are used throughout entire treatment</p>
+				<p className="text-small font-medium tracking-wide text-gray-900">Add ons: <b>Pumpkin Peel Mask</b> (+$20). Stubborn acne? Add on <b>High Frequency</b> (+$15).</p>
 				<p className="text-small font-medium tracking-wide text-gray-900">Price: $89 (60 Minutes)</p>
 			</div>
 
 			<div id="section-2" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">02</span>
 				<h3>O2 Facial</h3>
-				<p>Rejuvenate skin with oxygen infusion, plant-derived stem cells, peptides, and a high concentration of enzymatic botanicals.</p>
+				<p>Rejuvenate skin with oxygen infusion, plant-derived stem cells, peptides, and a high concentration of enzymatic botanicals. </p>
+				<p className="mb-3">Signature Facial that incorporates</p>
+				<ul className="styled-list mb-8">
+					<li>Image O2 Facial Products</li>
+					<li>Exfoliation</li>
+					<li>Mask treatment </li>
+				</ul>
+				<p>*Steam & hot towels are used throughout entire treatment.</p>
 				<p className="text-small font-medium tracking-wide text-gray-900">Price: $99 (60 Minutes)</p>
 			</div>
 
 			<div id="section-3" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">03</span>
 				<h3>Oxygen Facial</h3>
-				<p>Experience deep exfoliation without the downtime of a chemical peel! This exfoliating facial is not recommended for sensitive skin. Choose between lighting treatment for hyperpigmentation or rejuvenating treatment for fine lines and wrinkles.</p>
+				<p>Experience deep exfoliation without the downtime of a chemical peel! This exfoliating facial is <b>not recommended for sensitive skin</b>. Choose between NeoBright for a lighting treatment for hyper-pigmentation or NewRevive for rejuvenating treatment for fine lines and wrinkles.</p>
+				<p className="mb-3">Includes:</p>
+				<ul className="styled-list mb-8">
+					<li>Cleanse</li>
+					<li>Exfoliate with Sodium Bicarbonate Tip</li>
+					<li>Ultrasound for deeper product penetrations</li>
+					<li>Massage</li>
+					<li>Moisturize</li>
+				</ul>
+				<p>*Steam & hot towels are used throughout entire treatment</p>
+
+				<p className="text-small font-medium tracking-wide text-gray-900"><b>NeoBright active ingredients</b><br/>
+				Azelaic Acid: Evens out skin tone • Kojic Acid: Brightens • Retinol: Anti-aging
+				</p>
+
+				<p className="text-small font-medium tracking-wide text-gray-900"><b>NeoRevive active ingredients</b><br/>
+				Hyaluronic acid: Wrinkle reducer • Peptides: Skin repair • Retinol: Anti-aging
+				</p>				
 				<p className="text-small font-medium tracking-wide text-gray-900">Price: $119 (60 Minutes)</p>
 			</div> 
 
@@ -92,46 +130,88 @@ export const Page = ({data}) => {
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">04</span>
 				<h3>HydraFacial</h3>
 				<p>Get clearer skin through hydradermabrasion, which combines cleansing, exfoliation, extraction, hydration and antioxidant protection all at once. This treatment is soothing, moisturizing, non-invasive and non-irritating.</p>
-				<p className="text-small font-medium tracking-wide text-gray-900">Price: $145 (45 Minutes)</p>
+				<p className="mb-3">Includes:</p>
+				<ul className="styled-list mb-8">
+					<li>Cleanse</li>
+					<li>Exfoliation</li>
+					<li>Extraction</li>
+					<li>Hydration and Antioxidant Protection</li>
+				</ul>	
+				<p>*Steam & hot towels are used throughout entire treatment</p>
+				<p className="text-small font-medium tracking-wide text-gray-900">Price: $145 (60 Minutes)</p>
 			</div>  
 
 			<div id="section-5" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">05</span>
 				<h3>Back Facial</h3>
-				<p>We’ve got your back! Our back facial treatment includes Dr. Schwab Products, a steam session, cleansing, exfoliation, extractions, and finishing masks.</p>
+				<p>We’ve got your back! Benefit from our signature back facial that utilizes all-natural Dr. Schwab Products. Benefits include getting rid of back acne, reducing oiliness, and softening the skin and an overall soothing experience.</p>
+				<p>Consider adding on our Mini Facial to experience 60 minutes of relaxation.</p>
 				<p className="text-small font-medium tracking-wide text-gray-900">Price: $75 (30 Minutes)</p>
-			</div>  
+			</div> 
 
 			<div id="section-6" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">06</span>
+				<h3>Mini Facial</h3>
+				<p>A mini facial treatment is an abbreviated version of our signature facial treatment. Typically provided in 30 minutes, it’s short enough to complete during a lunch break but long enough to feel luxurious.</p>
+				<p className="mb-3">Includes:</p>
+				<ul className="styled-list mb-8">
+					<li>Skin analysis</li>
+					<li>Cleanse</li>
+					<li>Exfoliation</li>
+					<li>Facial Massage</li>
+					<li>Mask Treatment</li>
+					<li>Sunscreen</li>
+					<li>Toner & Moisturizer</li>
+					<li>Lip & Eye Treatment</li>
+				</ul>	
+				<p>*Steam & hot towels are used throughout entire treatment</p>
+				<p className="text-small font-medium tracking-wide text-gray-900">Price: $45 (30 Minutes)</p>
+			</div>  			
+
+			<div id="section-7" className="mb-20 lg:mb-40">
+				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">07</span>
 				<h3>Clarity & Led Therapy (Hillcrest only)</h3>
 				<p>Enjoy a full acne facial with pomegranate enzyme or cranberry turnover mask, and 20% salicylic acid. Full extractions with tea tree anti-acne jelly mask. 20 minutes LED acne light therapy.  </p>
 				<p className="text-small font-medium tracking-wide text-gray-900">Price: $140</p>
 			</div>
-			<div id="section-7" className="mb-20 lg:mb-40">
-				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">07</span>
+			<div id="section-8" className="mb-20 lg:mb-40">
+				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">08</span>
 				<h3>Customized Facial (Hillcrest only) </h3>
 				<p>Choose your mask and treatments! The facial comes with a double cleanse and customized treatment (pricing varies), extractions, facial massage, and customized mask. Choice of upgrading to add hydrojelly mask (pricing varies).</p>
 				<p className="text-small font-medium tracking-wide text-gray-900">Price: $69-$99 (45-50 Minutes)</p>
 			</div>
-			<div id="section-8" className="mb-20 lg:mb-40">
-				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">08</span>
-				<h3>Gentlemen’s Express Facial</h3>
-				<p>Deep pore cleanse and facial scrub to remove dead skin build up and remove any blackheads and reduce ingrown hair.  </p>
-				<p className="text-small font-medium tracking-wide text-gray-900">Price: $69</p>
-			</div>			
-
 			<div id="section-9" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">09</span>
+				<h3>Gentleman’s Facial </h3>
+				<p>Men need pampering too! This relaxing treatment helps repair the damage caused by daily shaving, skin sensitivity, dullness, and aging.</p>
+				<p className="mb-3">Includes:</p>
+				<ul className="styled-list mb-8">
+					<li>Skin analysis</li>
+					<li>Cleanse</li>
+					<li>Exfoliation</li>
+					<li>Extractions </li>
+					<li>Facial Massage</li>
+					<li>Mask Treatment</li>
+					<li>Arm & Hand Massage</li>
+					<li>Sunscreen</li>
+					<li>Toner & Moisturizer</li>
+					<li>Lip & Eye Treatment</li>
+				</ul>	
+				<p>*Steam & hot towels are used throughout entire treatment</p>
+				<p className="text-small font-medium tracking-wide text-gray-900">Price: $75 (60 Minutes)</p>
+			</div>			
+
+			<div id="section-10" className="mb-20 lg:mb-40">
+				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">10</span>
 				<h3>Waxing</h3>
-				<p>Remove unwanted hair and enjoy smooth skin with our waxing services. </p>
+				<p>Remove unwanted hair and enjoy smooth skin with our waxing services.</p>
 				<p className="text-small font-medium tracking-wide text-gray-900">
-					Brow: $25<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					Nose: $20<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					Upper Lip: $20<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
+					Brow: $15<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
+					Nose: $10<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
+					Upper Lip: $12<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Full Face: $50<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					Underarm: $30<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					Half Arm: $35<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
+					Underarm: $25<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
+					Half Arm: $30<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Full Arm: $45<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Half Leg: $55<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Full Leg: $75<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
@@ -139,50 +219,53 @@ export const Page = ({data}) => {
 					Full Back: $70<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Lower Back: $30<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Stomach: $40<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					Brazilian Wax: $60 (Includes back strip; trimming: add $20)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
+					Brazilian Wax: $60 (Includes back strip; trimming: add $10)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Bikini Line: $40<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Bikini Front: $50<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
 					Full Body: by consultation
 				</p>
 			</div>  
 
-			<div id="section-10" className="mb-20 lg:mb-40">
-				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">10</span>
-				<h3>Full Spectrum Infrared Sauna</h3>
-				<p>Spend some time in the sauna to look and feel great! Whether you want a traditional or infrared sauna experience, we’ve got you covered.</p>
-				<p className="text-small font-medium tracking-wide text-gray-900">
-					Infrared Sauna: <br className="block lg:hidden" />
-					$20 (15 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$35 (30 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$55 (60 Minutes)<br/><br className="block lg:hidden" />
-					Traditional Sauna: <br className="block lg:hidden" />
-					$15 (15 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$30 (30 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$40 (60 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$50 (90 Minutes)
-				</p>
-			</div> 
 			<div id="section-11" className="mb-20 lg:mb-40">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">11</span>
-				<h3>Sauna & Steam Room (SD)</h3>
+				<h3>Steam Room (Hillcrest Only)*</h3>
 				<p>Sweat, relax, and promote body detoxification at Karma Spa’s steam room. Sauna and  sessions help to relieve pain, relax muscles, and more.</p>
+				<p>*Carlsbad location only has a sauna.</p>
 				<p className="text-small font-medium tracking-wide text-gray-900">
-					$15 (15 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$20 (30 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$40 (60 Minutes)<br className="block lg:hidden" /><span className="hidden lg:inline lg:mx-1">•</span>
-					$50 (90 Minutes)
+					Price: $11 (15 Minutes) • $21 (30 Minutes) • $31 (60 Minutes)
 				</p>
 			</div>
 			<div id="section-12">
 				<span className="text-xs text-gray-900 font-bold tracking-widest block mb-6 lg:mb-8">12</span>
 				<div className="mb-20 lg:mb-40">
-					<h3>Glycolic Chemical Peel</h3>
+					<h3>Pumpkin Peel Mask</h3>
+					<p>This add-on helps drive highly refined extracted ingredients that have been added to (h2t) Pumpkin Peel (anti-oxidants, vitamins & nutrients) into the skin.</p>
+					<p className="mb-3">Benefits:</p>
+					<ul className="styled-list mb-8">
+						<li>Exfoliates surface cells</li>
+						<li>Helps destroy surface area bacteria </li>
+						<li>Binds moisture to the skin</li>
+						<li>Softens the appearance of fine lines and wrinkles</li>
+						<li>Improves texture & pliability of the corneum layer</li>
+					</ul>
+					<p className="text-small font-medium tracking-wide text-gray-900">Price: $20</p>
+				</div>				
+				<div className="mb-20 lg:mb-40">
+					<h3>Glycolic Chemical Peel (Hillcrest Only)</h3>
 					<p>Designed for closed comedones and keratinized aging skin with fine lines. Excellent booster to any enzyme.</p>
 					<p className="text-small font-medium tracking-wide text-gray-900">Price: $20</p>
 				</div>
 				<div className="mb-20 lg:mb-40">
 					<h3>High Frequency</h3>
-					<p>Treat and prevent stubborn acne, shrink enlarged pores, reduce inflammation, and decongest puffy eyes. Add to any facial.</p>
+					<p>Treat and prevent stubborn acne, shrink enlarged pores, reduce inflammation, and decongest puffy eyes. The primary action of high-frequency current is thermal (heat producing) and oxygen. Add to any facial.</p>
+					<p className="mb-3">Benefits:</p>
+					<ul className="styled-list mb-8">
+						<li>Assists in killing stubborn acne bacteria under the skin</li>
+						<li>Relieves cystic acne</li>
+						<li>Reduces the severity and duration of breakouts</li>
+						<li>Shrinks enlarged pores</li>
+						<li>Reduces inflammation</li>
+					</ul>
 					<p className="text-small font-medium tracking-wide text-gray-900">Price: $15</p>
 				</div>
 				<div className="mb-20 lg:mb-40">
