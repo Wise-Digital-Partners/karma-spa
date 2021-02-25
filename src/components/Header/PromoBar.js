e112707b50965f8f00094cec5d3052709b99fdae
hamletavetisian <55106549+hamletavetisian@users.noreply.github.com>
2021-02-25 10:24:14 -0800

    Update PromoBar.js

--- src/components/Header/PromoBar.js
+++ src/components/Header/PromoBar.js
@@ -4,7 +4,7 @@ const PromoBar = () => {
     return(
         <div id="promo-bar" className="bg-secondary_400 py-2 text-center">
             <div className="container">
-                <p className="text-white text-small md:text-base mb-0">Massage Deal for First-Timers: $49 for 60 min. | $69 for 90 min.<br className="block md:hidden"/> Call for details.</p>
+                <p className="text-white text-small md:text-base mb-0">Massage Deal for First-Timers: $55 for 60 min. | $69 for 90 min.<br className="block md:hidden"/> Call for details.</p>
             </div>
         </div>
     );
