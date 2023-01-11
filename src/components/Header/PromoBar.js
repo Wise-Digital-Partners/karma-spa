8973da639841f71654738367e96e347b7844718d
Sebastian <109978221+dev-sebastian-nash@users.noreply.github.com>
2023-01-11 11:24:34 -0800

    Update PromoBar.js

--- src/components/Header/PromoBar.js
+++ src/components/Header/PromoBar.js
@@ -7,9 +7,7 @@ const PromoBar = ({ scrolled }) => {
    return (
       <div id="promo-bar" className={`bg-secondary_400 py-2 text-center ${scrolled && "fixed top-0 left-0 w-full z-20"}`}>
          <div className="">
-            <PromoSlider >
- 
-            </PromoSlider>
+            <p className="text-white text-small md:text-base mb-0">Updated Hillcrest prices coming soon. Please call for up-to-date info!</p>
 
          </div>
       </div>
