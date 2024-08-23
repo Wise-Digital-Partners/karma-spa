a3706e9e19af00c3ed2b40c9ab853f411607f44b
Anibal Rojas <anibal@wisedigitalpartners.com>
2024-08-23 10:46:40 -0500

    FIX

--- src/components/Form/Contact.js
+++ src/components/Form/Contact.js
@@ -5,36 +5,36 @@ import tw from "twin.macro";
 import ButtonSolid from "../Button/ButtonSolid";
 
 function encode(data) {
-   return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 const StyledForm = styled.div`
-   form {
-      ${tw`flex flex-wrap`}
-   }
-   .field {
-      ${tw`mb-6 w-full`}
-   }
-   .label {
-      ${tw`block text-sm font-bold text-gray-800 mb-2`}
-   }
-   input[type="text"],
+  form {
+    ${tw`flex flex-wrap`}
+  }
+  .field {
+    ${tw`mb-6 w-full`}
+  }
+  .label {
+    ${tw`block text-sm font-bold text-gray-800 mb-2`}
+  }
+  input[type="text"],
   input[type="tel"],
   input[type="email"],
   /* select, */
   textarea {
-      ${tw`w-full px-4 py-3 border border-solid border-gray-200 rounded-sm`}
-      &:focus,
+    ${tw`w-full px-4 py-3 border border-solid border-gray-200 rounded-sm`}
+    &:focus,
     &:active {
-         ${tw`border-black outline-none`}
-      }
-   }
-   /* select {
+      ${tw`border-black outline-none`}
+    }
+  }
+  /* select {
     ${tw`h-12 appearance-none cursor-pointer`}
   } */
-   /* .select-wrapper {
+  /* .select-wrapper {
     ${tw`relative`}
     &:after {
       content: "\f078";
@@ -45,67 +45,80 @@ const StyledForm = styled.div`
 `;
 
 export default class Form extends Component {
-   constructor(props) {
-      super(props);
-      this.state = { isValidated: false };
-   }
+  constructor(props) {
+    super(props);
+    this.state = { isValidated: false };
+  }
 
-   handleChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value });
-   };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-   handleSubmit = (e) => {
-      e.preventDefault();
-      const form = e.target;
-      fetch("/", {
-         method: "POST",
-         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-         body: encode({
-            "form-name": form.getAttribute("name"),
-            ...this.state,
-         }),
-      })
-         .then(
-            () =>
-               (document.getElementById("contact-form-ajax-response").innerHTML =
-                  "Thank you for contacting us! We will get in touch with you shortly."),
-            form.remove(),
-            (window.dataLayer = window.dataLayer || []),
-            window.dataLayer.push({
-               event: "contactFormSubmission",
-            })
-         )
-         .catch((error) => alert(error));
-   };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(
+        () =>
+          (document.getElementById("contact-form-ajax-response").innerHTML =
+            "Thank you for contacting us! We will get in touch with you shortly."),
+        form.remove(),
+        (window.dataLayer = window.dataLayer || []),
+        window.dataLayer.push({
+          event: "contactFormSubmission",
+        })
+      )
+      .catch((error) => alert(error));
+  };
 
-   render() {
-      return (
-         <StyledForm>
-            <div id="contact-form-ajax-response"></div>
-            <form name="Contact" method="post" action="" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
-               {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-               <input type="hidden" name="form-name" value="Contact" />
-               <div hidden>
-                  <label>
-                     Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-               </div>
+  render() {
+    return (
+      <StyledForm>
+        <div id="contact-form-ajax-response"></div>
+        <form
+          name="Contact"
+          method="post"
+          action=""
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="Contact" />
+          <div hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </div>
 
-               <div className="field">
-                  <label className="label" htmlFor="name">
-                     Name
-                  </label>
-                  <input type="text" name="name" onChange={this.handleChange} id="name" required={true} />
-               </div>
+          <div className="field">
+            <label className="label" htmlFor="name">
+              Name
+            </label>
+            <input type="text" name="name" onChange={this.handleChange} id="name" required={true} />
+          </div>
 
-               {/* <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6 w-full"> */}
-               <div className="field">
-                  <label className="label" htmlFor="email">
-                     Email Address
-                  </label>
-                  <input type="email" name="email" onChange={this.handleChange} id="email" required={true} />
-               </div>
-               {/* <div className="field">
+          {/* <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-6 w-full"> */}
+          <div className="field">
+            <label className="label" htmlFor="email">
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+              id="email"
+              required={true}
+            />
+          </div>
+          {/* <div className="field">
               <label className="label" htmlFor="phone">Phone Number</label>
               <input
                   type="tel"
@@ -115,33 +128,51 @@ export default class Form extends Component {
                   required={true}
               />
           </div>  */}
-               {/* </div> */}
+          {/* </div> */}
 
-               <div className="field">
-                  <fieldset>
-                     <legend className="label">Select a Location</legend>
-                     <div className="inline-flex flex-col mt-2 mb-1">
-                        <label className="relative pl-8 mb-2 cursor-pointer">
-                           <input type="radio" name="location" value="Hillcrest" onChange={this.handleChange} required={true} />
-                           Hillcrest
-                           <span className="radio-checkmark"></span>
-                        </label>
-                        <label className="relative pl-8 mb-2 cursor-pointer">
-                           <input type="radio" name="location" onChange={this.handleChange} value="Carlsbad" />
-                           Carlsbad
-                           <span className="radio-checkmark"></span>
-                        </label>
-                     </div>
-                  </fieldset>
-               </div>
+          <div className="field">
+            <fieldset>
+              <legend className="label">Select a Location</legend>
+              <div className="inline-flex flex-col mt-2 mb-1">
+                <label className="relative pl-8 mb-2 cursor-pointer">
+                  <input
+                    type="radio"
+                    name="location"
+                    value="Hillcrest"
+                    onChange={this.handleChange}
+                    required={true}
+                  />
+                  Hillcrest
+                  <span className="radio-checkmark"></span>
+                </label>
+                <label className="relative pl-8 mb-2 cursor-pointer">
+                  <input
+                    type="radio"
+                    name="location"
+                    onChange={this.handleChange}
+                    value="Carlsbad"
+                  />
+                  Carlsbad
+                  <span className="radio-checkmark"></span>
+                </label>
+              </div>
+            </fieldset>
+          </div>
 
-               <div className="field">
-                  <label className="label" htmlFor="message">
-                     Message
-                  </label>
-                  <textarea className="textarea" name="message" onChange={this.handleChange} id="message" rows="4" required={true} />
-               </div>
-               {/* <div className="field">
+          <div className="field">
+            <label className="label" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              className="textarea"
+              name="message"
+              onChange={this.handleChange}
+              id="message"
+              rows="4"
+              required={true}
+            />
+          </div>
+          {/* <div className="field">
                <label className="relative mb-2 cursor-pointer text-xs">
                   <input type="checkbox" className="checkbox" name="disclaimer1" required={true} />
                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; By providing a telephone number and submitting the form, you are consenting to be contacted by SMS text message from Karma Spa. Message frequency may vary. Message & data rates may apply. Reply STOP to opt out of further messaging. Reply HELP for more information.
@@ -154,13 +185,11 @@ export default class Form extends Component {
                </label>
                </div> */}
 
-
-               
-               <div className="mt-2">
-                  <ButtonSolid as="button" type="submit" text="Submit" />
-               </div>
-            </form>
-         </StyledForm>
-      );
-   }
+          <div className="mt-2">
+            <ButtonSolid as="button" type="submit" text="Submit" />
+          </div>
+        </form>
+      </StyledForm>
+    );
+  }
 }
